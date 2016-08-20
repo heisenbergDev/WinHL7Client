1fbc9d2e3bb93003a192c210aad11978f4da4367
heisenbergDev <heisenberg.dev@gmail.com>
2016-08-20 11:26:17 +0200

    Initial commit.

---
+++ WinHL7Client/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using NHapi.Base.Model;
+using NHapi.Base.Parser;
+using NHapiTools.Base.Net;
+using System;
+using System.Net;
+using System.Windows;
+
+namespace WinHL7Client
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sendMessage(object sender, RoutedEventArgs e)
+        {
+            IMessage message = decodeHL7Message(messageContent.Text);
+            TcpConnDestinationData destinationConn = new TcpConnDestinationData(destinationIP.Text, destinationPort.Text);
+            messageReceivedTextbox.Text = destinationConn.ToString();
+
+            String ackResponse = sendHL7MessageToIpPort(destinationConn, message);
+
+            if (String.IsNullOrEmpty(ackResponse))
+            {
+                messageReceivedTextbox.Text = @"No message received!!";
+            }
+            else
+            {
+                messageReceivedTextbox.Text = ackResponse;
+
+            }
+
+        }
+
+        private String sendHL7MessageToIpPort(TcpConnDestinationData destinationConn, IMessage message)
+        {
+            String ackReceived = null;
+
+            if ((destinationConn.isValid()) && (message != null))
+            {
+                SimpleMLLPClient mllpClient = new SimpleMLLPClient(destinationConn.Ip, destinationConn.Port);
+                IMessage response = mllpClient.SendHL7Message(message);
+                ackReceived = encodeHL7Message(response);
+                mllpClient.Disconnect();
+            }
+            return ackReceived;
+        }
+
+        private String encodeHL7Message(IMessage iMessage)
+        {
+            PipeParser parser = new PipeParser();
+            String encodedMessage = null;
+
+            if (iMessage != null) encodedMessage = parser.Encode(iMessage);
+
+            return encodedMessage;
+        }
+
+        private IMessage decodeHL7Message(String message)
+        {
+            PipeParser parser = new PipeParser();
+            IMessage decodedMessage = null;
+
+            if (!String.IsNullOrEmpty(message)) decodedMessage = parser.Parse(message);
+
+            return decodedMessage;
+        }
+    }
+}
---
+++ WinHL7Client/TcpConnDestinationData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinHL7Client
+{
+    class TcpConnDestinationData
+    {
+        public String Ip { get; set; }
+
+        public int Port { get; set; }
+
+        public TcpConnDestinationData(String Ip, String Port)
+        {
+            int portNumber;
+            if (Int32.TryParse(Port, out portNumber) && validateIpAndPort(Ip, portNumber))
+            {
+                init(Ip, portNumber);
+            }
+
+        }
+
+        private void init(String Ip, int Port)
+        {
+
+            this.Ip = Ip;
+            this.Port = Port;
+        }
+
+        public Boolean isValid()
+        {
+            if (validateIpAndPort(this.Ip, this.Port))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        private Boolean validateIpAndPort(String Ip, int Port)
+        {
+            if (checkIpAddress(Ip) && (Port > 0) && (Port < 65536))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        private Boolean checkIpAddress(String ip)
+        {
+            IPAddress ipParsed;
+            if (!(String.IsNullOrEmpty(ip)) && (IPAddress.TryParse(ip, out ipParsed)))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override String ToString()
+        {
+            return this.Ip + " " + this.Port;
+        }
+    }
+}
